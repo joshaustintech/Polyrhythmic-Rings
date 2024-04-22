a8696575d83bbda07fd0163d056f05e06f54a78f
Josh Austin <83935880+joshaustintech@users.noreply.github.com>
2024-04-21 21:48:43 -0500

    Initial Project Creation

---
+++ Polyrhythmic Rings/Polyrhythmic Rings/Polyrhythmic_RingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polyrhythmic_RingsApp.swift
+//  Polyrhythmic Rings
+//
+//  Created by Joshua Austin on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Polyrhythmic_RingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
